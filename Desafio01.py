b34a883907a638806657a6aa754ec575b0c6724c
GMSANTANA <30562400+GMSANTANA@users.noreply.github.com>
2017-07-30 01:03:51 -0300

    Add files via upload

---
+++ Desafio01.py
@@ -0,0 +1,4 @@
+#
+menssagem01 = "Gabriel"
+menssagem02 = "Santana"
+print("Olá, " + menssagem01 + " " + menssagem02 + " !! ")
---
+++ Desafio02.py
@@ -0,0 +1,9 @@
+# Armazenar uma string 'exemplo ' em uma variavel com espaço em branco, depois exibir
+# o resultado na tela
+
+nome = 'exemplo '
+print(nome.rstrip())  # para a direita
+nome2 = ' exemplo'
+print(nome2.lstrip()) # para a esqueda
+nome3 = ' exemplo '
+print(nome3.strip())  # para ambos os lados
