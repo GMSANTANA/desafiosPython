cb63ae7524d03f838b953c23329afc135670e0fb
Gabriel Santana <30562400+GMSANTANA@users.noreply.github.com>
2017-08-03 13:42:36 -0300

    Delete desafio11.py

--- desafio11.py
+++
@@ -1,5 +0,0 @@
-# Crie um script que substitua o primeiro item de sua lista, mostre os resultados na tela antes, e depois de alterar a lista.
-
-lista = ['yamaha', 'suzuki', 'honda']
-print(lista)
-print(lista[0])
