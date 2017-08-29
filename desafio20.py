897501570ad5d6b9803f79a07872705cb24caf81
Gabriel Santana <30562400+GMSANTANA@users.noreply.github.com>
2017-08-29 02:34:40 -0300

    Add files via upload

---
+++ desafio20.py
@@ -0,0 +1,16 @@
+# Crie um script com uma lista depois armazene um elemento da lista, em uma variavel
+# ,remova o elemento da lista, com o método .remove() faça a concatenação usando o segundo
+# valor da variavel, com uma frase "string" observe que o remove apenas elimina a primera ocorrencia
+# explique por que isso acontece.
+
+carros = ['hb20', 'onix', 'ka+', 'golf']
+muito_caro = 'golf'
+carros.remove(muito_caro)
+print("Esse " + muito_caro.title() + " e muito caro para min!! ")
+
+#  o remove()
+# exclui apenas a primeira ocorrência do valor que você especificou. 
+
+
+
+
