62b25073d7835625864ae88f081798e9385341c2
Gabriel Santana <30562400+GMSANTANA@users.noreply.github.com>
2017-08-29 02:17:22 -0300

    Add files via upload

---
+++ desafio19.py
@@ -0,0 +1,4 @@
+# Explique quando usar o del e quando usar o pop()
+
+# Quando utilizarmos o del vamos deletar definitivamente na lista, e quando usarmos o pop() 
+# vamos deletar os dados mas vamos poder utilizalos novamente. 
