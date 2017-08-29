b5f6e83e48377ca5545376ecc3ee2a0ef028a65e
Gabriel Santana <30562400+GMSANTANA@users.noreply.github.com>
2017-08-29 02:10:32 -0300

    Add files via upload

---
+++ desafio18.py
@@ -0,0 +1,8 @@
+# Fazer o mesmo que o 17 mas na posição que eu desejo
+
+carros = ['hb20', 'onix', 'ka+', 'golf']
+print(carros)
+
+carros_batidos = carros.pop(0)
+
+print("O carro batido foi o " + carros_batidos + " !! ")
